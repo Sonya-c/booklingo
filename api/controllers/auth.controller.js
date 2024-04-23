2a2d84a86046c138526b2297a208a3f6c241a5be
Sonya <sonya.castro.gomez@gmail.com>
2024-04-23 14:38:58 -0500

    feat: add unit test for auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+
 const { validationResult } = require("express-validator");
 const status = require('http-status');
 
---
+++ test/unit/auth.test.js
@@ -0,0 +1,154 @@
+require("dotenv").config();
+
+const status = require('http-status');
+const request = require("supertest");
+const app = require("../../app");
+
+
+describe("POST /auth/login", () => {
+
+    it("Given no email and no password, it should return an error.", async () => {
+        const res = await request(app).post("/auth/login");
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given no password, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/login")
+            .send({
+                "email": "email@domain.com"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given no email, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/login")
+            .send({
+                "password": "1234567"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given wrong email and wrong password, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/login")
+            .send({
+                "email": "a@a.a",
+                "password": "123"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given wrong email, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/login")
+            .send({
+                "email": "a@a.a",
+                "password": "12345678"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given wrong password, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/login")
+            .send({
+                "email": "email@domain.com",
+                "password": "123"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given good email and password, it should return an user and a jwt.", async () => {
+        const email = "email@domain.com";
+
+        const res = await request(app)
+            .post("/auth/login")
+            .send({
+                email,
+                "password": "123456789"
+            });
+
+        expect(res.statusCode).toBe(status.OK);
+        expect(res.body.user.email).toBe(email);
+        expect(res.body.token).toBeDefined();
+    });
+});
+
+
+describe("POST /auth/register", () => {
+
+    it("Given no email and no password, it should return an error.", async () => {
+        const res = await request(app).post("/auth/register");
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given no password, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/register")
+            .send({
+                "email": "email@domain.com"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given no email, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/register")
+            .send({
+                "password": "1234567"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given wrong email and wrong password, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/register")
+            .send({
+                "email": "a@a.a",
+                "password": "123"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given wrong email, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/register")
+            .send({
+                "email": "a@a.a",
+                "password": "12345678"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given wrong password, it should return an error.", async () => {
+        const res = await request(app)
+            .post("/auth/register")
+            .send({
+                "email": "email@domain.com",
+                "password": "123"
+            });
+        expect(res.statusCode).toBe(status.UNPROCESSABLE_ENTITY);
+    });
+
+    it("Given good email and password, it should return an user and a jwt.", async () => {
+        const email = "email@domain.com";
+
+        const res = await request(app)
+            .post("/auth/register")
+            .send({
+                email,
+                "password": "123456789"
+            });
+
+        expect(res.statusCode).toBe(status.CREATED);
+        expect(res.body.user.email).toBe(email);
+        expect(res.body.token).toBeDefined();
+    });
+});
+
+
+describe("POST /auth/logout", () => {
+    // TODO: logout test 
+});
